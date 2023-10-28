45ab666283045776d11e3e80c185b7e8815f1835
Jesse-Calebe <jcalebe18@gmail.com>
2023-10-28 15:07:09 -0300

    ajuste jogo da vida

--- 1_JogoDaVida/main.js
+++ 1_JogoDaVida/main.js
@@ -3,17 +3,14 @@ function buildGrid() {
   const container = document.getElementById("grid-container");
 
   function makeRows(cols, cells) {
-    container.style.setProperty("--grid-cols", cols);
-    container.style.setProperty("--grid-cells", cells);
-
     for (i = 0; i < cols; i++) {
       let col = document.createElement("div");
-      col.className = 'col';
+      col.className = "col";
       col.id = `col${i}`;
 
       for (j = 0; j < cells; j++) {
         let cell = document.createElement("div");
-        cell.className = 'cell';
+        cell.className = "cell";
         cell.id = `cell${i}x${j}`;
         cell.onclick = cellClick;
 
@@ -30,9 +27,8 @@ function buildGrid() {
 function cellClick(oEvent) {
   let id = oEvent.currentTarget.id;
 
-  document.getElementById(id).style.setProperty("background-color", "blue")
+  document.getElementById(id).style.setProperty("background-color", "blue");
 }
-
 
 // Events
 window.onload = buildGrid;
